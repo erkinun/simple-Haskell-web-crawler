5cacf86287dc944d3a48c1e973e655883163a3fb
lost_mohican <erkinun@gmail.com>
2015-01-17 22:19:51 +0200

    argument is checked

--- Crawler.hs
+++ Crawler.hs
@@ -1,3 +1,13 @@
+import System.Environment
 
 main = do 
-  print "Crawler Main"
+  print "Crawler Main starting..."
+  urls <- getArgs
+  return $ checkUrls urls
+  print $ head urls ++ " will be crawled"
+
+checkUrls :: [String] -> ()
+checkUrls [] = error "supply an argument"
+checkUrls urls 
+          | length urls > 1 = error "supply just one url"
+          | otherwise = ()
