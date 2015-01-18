0f01d202aeb686fd8120370b5d0dd946ebdc1e60
lost_mohican <erkinun@gmail.com>
2015-01-18 23:10:53 +0200

    added simple http

--- Crawler.hs
+++ Crawler.hs
@@ -1,10 +1,17 @@
 import System.Environment
+import Network.HTTP
+import Network.Browser
 
 main = do 
   print "Crawler Main starting..."
   urls <- getArgs
   return $ checkUrls urls
   print $ head urls ++ " will be crawled"
+  (_, rsp)
+         <- Network.Browser.browse $ do
+               setAllowRedirects True -- handle HTTP redirects
+               request $ getRequest $ head urls
+  print $ rspBody rsp
 
 checkUrls :: [String] -> ()
 checkUrls [] = error "supply an argument"
