18bb4cd171b18bda76abbfe290e592708ed46566
lost_mohican <erkinun@gmail.com>
2015-01-21 23:58:05 +0200

    adding parsing links

--- Crawler.hs
+++ Crawler.hs
@@ -11,10 +11,13 @@ main = do
          <- Network.Browser.browse $ do
                setAllowRedirects True -- handle HTTP redirects
                request $ getRequest $ head urls
-  print $ rspBody rsp
+  print $ getLinks $ rspBody rsp
 
 checkUrls :: [String] -> ()
 checkUrls [] = error "supply an argument"
 checkUrls urls 
           | length urls > 1 = error "supply just one url"
           | otherwise = ()
+
+getLinks :: String -> [String]
+getLinks str = [str]
