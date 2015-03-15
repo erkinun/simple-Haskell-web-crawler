4d85e960add66ad22aee50e050a88f45c857e9b1
lost_mohican <erkinun@gmail.com>
2015-03-15 17:43:24 +0200

    get page content encapsulated as a function

--- Crawler.hs
+++ Crawler.hs
@@ -1,18 +1,20 @@
 import System.Environment
 import Network.HTTP
+import Network.URI
 import Network.Browser
 
 main = do 
   print "Crawler Main starting..."
   print "Please type in a url to crawl: "
   urls <- getLine
+
   return $ checkUrls [urls]
   print $ urls ++ " will be crawled"
-  (_, rsp)
-         <- Network.Browser.browse $ do
-               setAllowRedirects True -- handle HTTP redirects
-               request $ getRequest $ urls
+
+  (_, rsp) <- getPageContent urls
+
   print $ getLinks $ rspBody rsp
+  main
 
 checkUrls :: [String] -> ()
 checkUrls [] = error "supply an argument"
@@ -22,3 +24,8 @@ checkUrls urls
 
 getLinks :: String -> [String]
 getLinks str = [str]
+
+getPageContent :: String -> IO (Network.URI.URI, Response String)
+getPageContent url = Network.Browser.browse $ do
+               setAllowRedirects True -- handle HTTP redirects
+               request $ getRequest $ url
